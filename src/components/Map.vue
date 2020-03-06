ba7d8c43d89b85052f290775d09e5197d61f1563
Niek125 <niek.sleddens@gmail.com>
2020-03-06 13:01:30 +0100

    styling fixes popup

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,8 +1,19 @@
 <template>
     <MglMap :accessToken="accessToken" :mapStyle="mapStyle" @load="onMapLoaded">
-        <MglMarker :coordinates="coordinates">
-            <MglPopup>
-                <h1> Hello there</h1>
+        <MglMarker :coordinates="coordinates" color="green">
+            <MglPopup :closeButton="false" :offset="{
+                'top': [0, 0],
+                 'top-left': [0,0],
+                  'top-right': [0,0],
+                   'bottom': [0, -markerHeight],
+                    'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+                     'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+                      'left': [markerRadius, (markerHeight - markerRadius) * -1],
+                       'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+            }">
+                <v-card class="green lighten-1">
+                    <v-card-text>I'm food</v-card-text>
+                </v-card>
             </MglPopup>
         </MglMarker>
     </MglMap>
@@ -18,7 +29,10 @@
             return {
                 accessToken: "pk.eyJ1IjoibGV2aTEyNSIsImEiOiJjazdmemF0YmgwNGdoM25sMWlkdW5odHFtIn0.j7r078aeYv8NyC6k9-L-Kg",
                 mapStyle: "mapbox://styles/mapbox/streets-v11",
-                coordinates: [-111.549668, 39.014]
+                coordinates: [20.549668, -5.014],
+                markerHeight: 23,
+                markerRadius: 10,
+                linearOffset: 0,
             }
         },
         methods: {
@@ -36,6 +50,16 @@
         },
     }
 </script>
+
+<style>
+    .mapboxgl-popup-content {
+        padding: 0px !important;
+    }
+
+    .mapboxgl-popup-tip {
+        display: none;
+    }
+</style>
 
 <style scoped>
 
