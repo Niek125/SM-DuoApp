ba7838b838555257af0ecaadd4c0fb5036230b7e
Niek125 <niek.sleddens@gmail.com>
2020-03-06 11:18:36 +0100

    added mapbox

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1>Map</h1>
+    <MglMap :accessToken="accessToken" :mapStyle="mapStyle" @load="onMapLoaded"/>
 </template>
 
 <script>
+    import Mapbox from "mapbox-gl";
+    import {MglMap} from "vue-mapbox";
+
     export default {
-        name: "Map"
+        name: "Map",
+        data() {
+            return {
+                accessToken: "pk.eyJ1IjoibGV2aTEyNSIsImEiOiJjazdmemF0YmgwNGdoM25sMWlkdW5odHFtIn0.j7r078aeYv8NyC6k9-L-Kg",
+                mapStyle: "mapbox://styles/mapbox/streets-v11",
+            }
+        },
+        methods: {
+            onMapLoaded(event) {
+                this.map = event.map;
+            }
+        },
+        created() {
+            this.mapbox = Mapbox;
+        },
+        components: {
+            MglMap
+        },
     }
 </script>
 
