af6579b38332b3433746c759987fe3aec6fa7f8b
Niek125 <niek.sleddens@gmail.com>
2020-03-06 12:04:49 +0100

    marker with pop up

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,16 @@
 <template>
-    <MglMap :accessToken="accessToken" :mapStyle="mapStyle" @load="onMapLoaded"/>
+    <MglMap :accessToken="accessToken" :mapStyle="mapStyle" @load="onMapLoaded">
+        <MglMarker :coordinates="coordinates">
+            <MglPopup>
+                <h1> Hello there</h1>
+            </MglPopup>
+        </MglMarker>
+    </MglMap>
 </template>
 
 <script>
     import Mapbox from "mapbox-gl";
-    import {MglMap} from "vue-mapbox";
+    import {MglMap, MglMarker, MglPopup} from "vue-mapbox";
 
     export default {
         name: "Map",
@@ -12,6 +18,7 @@
             return {
                 accessToken: "pk.eyJ1IjoibGV2aTEyNSIsImEiOiJjazdmemF0YmgwNGdoM25sMWlkdW5odHFtIn0.j7r078aeYv8NyC6k9-L-Kg",
                 mapStyle: "mapbox://styles/mapbox/streets-v11",
+                coordinates: [-111.549668, 39.014]
             }
         },
         methods: {
@@ -23,7 +30,9 @@
             this.mapbox = Mapbox;
         },
         components: {
-            MglMap
+            MglMap,
+            MglMarker,
+            MglPopup,
         },
     }
 </script>
